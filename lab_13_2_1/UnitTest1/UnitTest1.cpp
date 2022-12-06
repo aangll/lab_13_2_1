0d90b61f1239a9ad30e1fb51f9eeab859c48650e
aangll <115033039+aangll@users.noreply.github.com>
2022-12-06 14:37:07 +0200

    Add files via upload

---
+++ lab_13_2_1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+#include "../lab_13_2_1/Source.cpp"
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = main();
+			Assert::AreEqual(t, 0);
+		}
+	};
+}
